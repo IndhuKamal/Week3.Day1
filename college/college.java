f0a340570c27614d28668848f741d52770bc042d
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-06-27 12:15:01 +0800

    Add files via upload

---
+++ college/college.java
@@ -0,0 +1,14 @@
+package org.college;
+
+
+public class college {
+public void CollegeName() {
+	System.out.println("A.V.C. College of Engineering");
+}
+public void CollegeCode() {
+	System.out.println("AVC898890");
+}
+public void CollegeRank() {
+	System.out.println("Top Rank");
+}
+}
