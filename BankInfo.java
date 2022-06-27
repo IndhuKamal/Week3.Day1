279740b5c1788d6e1934789343a1d6613e63bdbf
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-06-27 12:17:23 +0800

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,14 @@
+package week3.Day1;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Savings Acc");
+	}
+	public void fixed() {
+		System.out.println("Fixed Acc");
+	}
+	public void deposit() {
+		System.out.println("Deposit Acc");
+
+}
+}
