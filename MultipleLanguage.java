4c7c388d5df2ec30a81a8c3c038015ad7461f928
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-06-27 12:19:37 +0800

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,8 @@
+package week3.Day1;
+
+public abstract class MultipleLanguage implements Language,TestTool {
+public void Python() {
+	System.out.println("I am learning Python");
+}
+public abstract void ruby();
+}
